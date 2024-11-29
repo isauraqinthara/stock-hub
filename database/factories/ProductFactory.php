63c777643a4e4ea854364b88f8b9941a1f2898e3
Ilhammzz <ilham.abdulaziz20@gmail.com>
2024-11-29 15:12:51 +0700

    test

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,15 @@
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word(),
+            'quantity' => $this->faker->numberBetween(1, 100),
+            'price' => $this->faker->randomFloat(2, 1, 100),
+        ];
+    }
+}
