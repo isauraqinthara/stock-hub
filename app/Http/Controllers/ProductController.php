9e4f8241d689d81cabde4548a9e5c2f3e086a4d8
Ilhammzz <ilham.abdulaziz20@gmail.com>
2024-11-29 09:08:57 +0700

    fix controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,18 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index(): \Illuminate\View\View
+    public function index(): \Illuminate\Contracts\View\View
     {
         $products = Product::all();
         return view('products.index', compact('products'));
     }
 
-    public function create(): \Illuminate\View\View
+    public function create(): \Illuminate\Contracts\View\View
     {
         return view('products.create');
     }
@@ -31,7 +30,7 @@ class ProductController extends Controller
         return redirect()->route('products.index')->with('success', 'Product added successfully');
     }
 
-    public function edit(Product $product):  \Illuminate\View\View
+    public function edit(Product $product):  \Illuminate\Contracts\View\View
     {
         return view('products.edit', compact('product'));
     }
