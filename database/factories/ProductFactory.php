68c9e93cd989db333b6251b95434cf4d71ad0748
Ilhammzz <ilham.abdulaziz20@gmail.com>
2024-12-06 13:44:59 +0700

    factory fix

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -1,17 +1,26 @@
 <?php
 
+namespace Database\Factories;
+
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
 class ProductFactory extends Factory
 {
-    protected $model = Product::class;
-
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
     public function definition()
     {
         return [
-            'name' => $this->faker->word(),
+            'name' => $this->faker->word,
             'quantity' => $this->faker->numberBetween(1, 100),
-            'price' => $this->faker->randomFloat(2, 1, 100),
+            'price' => $this->faker->randomFloat(2, 10, 1000),
+            //
         ];
     }
 }
