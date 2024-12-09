a4a140f52b77e3d0551571e0eca5402f61540230
Ilhammzz <ilham.abdulaziz20@gmail.com>
2024-12-09 12:10:28 +0700

    testing laravel dusk

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -8,16 +8,17 @@ use Tests\DuskTestCase;
 
 class ExampleTest extends DuskTestCase
 {
-    /**
-     * A basic browser test example.
-     *
-     * @return void
+  /**
+     * Test to check if the home page displays "Stock Management".
      */
     public function testBasicExample()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('Laravel');
+            $browser->visit('/') // Akses halaman utama
+                    ->assertSee('Stock Management') // Periksa teks "Stock Management" pada halaman
+                    ->assertSee('Product List') // Periksa teks "Product List"
+                    ->assertPresent('a[href="http://127.0.0.1:8000/products/create"]') // Periksa link "Add New Product"
+                    ->assertSeeIn('table thead tr', 'Name'); // Periksa apakah tabel memiliki kolom "Name"
         });
     }
 }
